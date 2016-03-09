4b1e0a551a78e40680b79b25e1caa2d47029a3c5
opus1269 <igreilly1269@gmail.com>
2016-03-09 06:47:59 -0600

    linear easing - working well, finally

--- src/com/weebly/opus1269/smoothscroller/SmoothScrollerMouseWheelListener.java
+++ src/com/weebly/opus1269/smoothscroller/SmoothScrollerMouseWheelListener.java
@@ -34,11 +34,11 @@ class SmoothScrollerMouseWheelListener implements MouseWheelListener, ActionList
 
     public static final float MAX_SPEED_THRESHOLD = 0.001F;
     public static final float MAX_SPEED_LMT = 1.0F;
-    public static final float MAX_FRIC_EXP = 4.0F;
+    public static final float MAX_FRIC_EXP = 0.1F;
 
     public static final float DEF_SPEED_THRESHOLD = 0.0005F;
     public static final float DEF_SPEED_LMT = 0.5F;
-    public static final float DEF_FRIC_EXP = 1.1F;
+    public static final float DEF_FRIC_EXP = 0.05F;
 
     private static float sSpeedThreshold = DEF_SPEED_THRESHOLD;
     private static float sSpeedLmt = DEF_SPEED_LMT;
@@ -56,7 +56,7 @@ class SmoothScrollerMouseWheelListener implements MouseWheelListener, ActionList
 
     private double mVelocity = 0.0D;
     private ArrayList<Double> mVelocities = new ArrayList<Double>();
-    private static final int MAX_VELOCITIES = 5;
+    private static final int MAX_VELOCITIES = 10;
 
     /**
      * Constructor for our MouseWheelListener.
@@ -107,8 +107,8 @@ class SmoothScrollerMouseWheelListener implements MouseWheelListener, ActionList
         // calculate average velocity over last several mouse wheel events
         double scrollDelta = e.getScrollAmount() * wheelDelta;
         final double newVelocity = scrollDelta / elapsedMillis;
-        // skip small movements
-        if (Math.abs(newVelocity) > sSpeedThreshold) {
+        // skip small or reverse movements
+        if (Math.abs(newVelocity) > sSpeedThreshold && mVelocity * newVelocity >= 0.0D) {
             if (mVelocities.size() == MAX_VELOCITIES) {
                 mVelocities.remove(0);
             }
@@ -116,6 +116,10 @@ class SmoothScrollerMouseWheelListener implements MouseWheelListener, ActionList
         }
 
         mVelocity = getAverage(mVelocities);
+//        if (mVelocity * wheelDelta >= 0.0D) {
+//            mVelocity = mVelocity + wheelDelta * (double) elapsedMillis;
+//        }
+
 
 //        // apply speed limit
 //        if (Math.abs(mVelocity) > sSpeedLmt) {
@@ -164,17 +168,19 @@ class SmoothScrollerMouseWheelListener implements MouseWheelListener, ActionList
 
         if (!mScrolling) {
             // Basic kinetic scrolling, exponential decay vel_new = vel * e^-lambda*deltaT
-            mVelocity = mVelocity * Math.exp(-sFricExp * elapsedMillis);
+            // mVelocity = mVelocity * Math.exp(-sFricExp * elapsedMillis);
+            // basic kinetic scrolling, linear easing
+            mVelocity = mVelocity - sFricExp * mVelocity;
+        }
 
-            if (Math.abs(mVelocity) >= sSpeedThreshold) {
-                // reposition cursor offset based on current velocity
-                final int currentOffset = mScrollingModel.getVerticalScrollOffset();
-                final long offset = Math.round((currentOffset + mVelocity * elapsedMillis));
-                mScrollingModel.scrollVertically(Math.max(0, (int) offset));
-            } else {
-                // bring to stop below threshold
-                mVelocity = 0.0D;
-            }
+        if (Math.abs(mVelocity) >= sSpeedThreshold) {
+            // reposition cursor offset based on current velocity
+            final int currentOffset = mScrollingModel.getVerticalScrollOffset();
+            final long offset = Math.round((currentOffset + mVelocity * elapsedMillis));
+            mScrollingModel.scrollVertically(Math.max(0, (int) offset));
+        } else {
+            // bring to stop below threshold
+            mVelocity = 0.0D;
         }
     }
 
